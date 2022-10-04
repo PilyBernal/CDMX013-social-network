84ab9032b63895f195e5a3d305066c713291a5e8
Mmvs487 <monicavela@ciencias.unam.mx>
2022-10-04 11:08:26 -0500

    Agega esbozo de generador de posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,18 @@ import { existingUserAccess } from '../lib/auth.js';
 
 export function Home() {
 
-  const div = document.createElement('div');
+  const homeContent = document.createElement('div');
+  homeContent.classList.add('homeContent')
+  const lotoBoxContainer = document.createElement('div');
+  lotoBoxContainer.classList.add('lotoBoxContainer');
+  const postAQuestion = document.createElement('label');
+  postAQuestion.classList.add('postAQuestion')
+  postAQuestion.textContent = 'Ask your botany-related question!'
+  lotoBoxContainer.append(postAQuestion)
   const title = document.createElement('h5');
   const title2 = document.createElement('h5');
-
-  title.textContent = 'Welcome home !!!';
-  title2.textContent = 'In construction !!!';
   
-  div.append(title, title2);
+  homeContent.append(lotoBoxContainer);
 
-  return div;
+  return homeContent;
 }
